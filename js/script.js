bfba6f316460b06c985a9b8f3b75fee276fd91a7
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-03-19 16:19:38 +0100

    eliminazione console.log

--- js/script.js
+++ js/script.js
@@ -3,32 +3,24 @@
 // Chiedere numero di chilometri e l'età
 var chilometri = prompt('Quanti chilometri devi percorrere?');
 var etaUtente = prompt('Inserisci la tua età');
-console.log(chilometri);
-console.log(etaUtente);
 
 // Trasformare dati in numeri
 var chilometriNumber = parseInt(chilometri);
 var etaUtenteNumber = parseInt(etaUtente);
-console.log(chilometriNumber);
-console.log(etaUtenteNumber);
 
 // Calcolare prezzo senza sconti
 var prezzoAlChilometro = 0.21;
 var prezzoBiglietto = chilometriNumber * prezzoAlChilometro;
-console.log(prezzoBiglietto);
 
 // Calcolare prezzo con sconti
 if ( etaUtente < 18 ) {
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100);
-    console.log(prezzoBiglietto);
 } else if ( etaUtente > 65 ) {
     prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40 / 100);
-    console.log(prezzoBiglietto);
 }
 
 // Stampare prezzo con massimo due decimali
 var prezzoBigliettoFinale = prezzoBiglietto.toFixed(2);
-console.log(prezzoBigliettoFinale);
 
 var messaggio = 'Il prezzo del tuo biglietto è: ' + prezzoBigliettoFinale + '€';
 
